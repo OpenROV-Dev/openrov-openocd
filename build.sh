265b19995d4ab8afc75cf6fc25d1c16002a43365
Charles Cross <charles@openrov.com>
2016-03-15 10:39:53 -0700

    Fixed installation path issue that was causing openocd to have wrong default paths

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ set -e
 
 apt-get install libtool libusb-1.0-0-dev autoconf pkg-config
 
-mkdir -p output/usr/local/
+mkdir -p output/usr
 
-OPENOCD_DIR=${PWD}/output/usr/local
+OPENOCD_DIR=${PWD}/output/usr
 
 # Build openocd
 git clone http://repo.or.cz/openocd.git
 cd openocd
 ./bootstrap
-./configure --prefix=${OPENOCD_DIR} --enable-sysfsgpio --enable-maintainer-mode --enable-bcm2835gpio
+./configure --prefix=/usr/ --enable-sysfsgpio --enable-maintainer-mode --enable-bcm2835gpio
 make -j8
-make install
+make install DESTDIR=${OPENOCD_DIR}
 cd ..
 rm -rf openocd
 
